be9dc2d3effab3cc196fbdd414666626c3ef289d
Krrish Dewangan <dewangankrrish50@gmail.com>
2026-09-04 11:18:10 +0530

    chore: hide patient entry on landing page

--- src/lib/config/navigation.ts
+++ src/lib/config/navigation.ts
@@ -1,5 +1,5 @@
 export const publicNavigationConfig = {
-  showPatientEntry: true,
+  showPatientEntry: false,
   showWorkerEntry: true,
   showDistrictEntry: true,
   primaryAudience: "public-health"
